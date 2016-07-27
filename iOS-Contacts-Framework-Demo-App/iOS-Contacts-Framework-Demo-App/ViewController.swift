0130438d6cb11799491b8ccf457c924d2ecc0fec
Alexandru Schwartz <alexandru.schwartz@softvision.ro>
2016-07-27 17:49:41 +0300

    Initial commit

---
+++ iOS-Contacts-Framework-Demo-App/iOS-Contacts-Framework-Demo-App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS-Contacts-Framework-Demo-App
+//
+//  Created by Alexandru Schwartz on 27/07/16.
+//  Copyright © 2016 Alexandru Schwartz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
