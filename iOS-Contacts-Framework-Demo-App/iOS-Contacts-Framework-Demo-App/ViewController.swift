21b61032c626ce01289f3277ba9e3003fc47524d
Alexandru Schwartz <alexandru.schwartz@softvision.ro>
2016-07-27 19:02:05 +0300

    Parially implemented UI part

--- iOS-Contacts-Framework-Demo-App/iOS-Contacts-Framework-Demo-App/ViewController.swift
+++ iOS-Contacts-Framework-Demo-App/iOS-Contacts-Framework-Demo-App/ViewController.swift
@@ -7,19 +7,71 @@
 //
 
 import UIKit
+import Contacts
+import ContactsUI
 
-class ViewController: UIViewController {
+enum PilotNumberType {
+    case Inbound
+    case Outbound
+    case Both
+}
 
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    @IBOutlet weak var pilotNumberTypePicker: UIPickerView!
+    @IBOutlet weak var contactNameTextView: UITextView!
+    @IBOutlet weak var inboundPilotNumberLabel: UILabel!
+    @IBOutlet weak var outboundPilotNumberLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.pilotNumberTypePicker.showsSelectionIndicator = true
+        self.pilotNumberTypePicker.selectRow(0, inComponent: 0, animated: false)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    // MARK: - Actions
+
+    @IBAction func showDeviceContactsTaped(sender: AnyObject, forEvent event: UIEvent) {
+        let controller = CNContactPickerViewController()
+        self.presentViewController(controller, animated: true) { 
+            print("CNContactPickerViewController vas presented.")
+        }
     }
-
-
+    
+    @IBAction func decorateContactButtonTapped(sender: AnyObject) {
+        print("Selected contact type is \(self.pilotNumberTypePicker.selectedRowInComponent(0))")
+    }
+    
+    // MARK: UIPickerViewDataSource
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return 3
+    }
+    
+    // MARK: UIPickerViewDelegate
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        let title : String?
+        
+        switch row {
+        case 0:
+            title = "Inbound"
+        case 1:
+            title = "Outbound"
+        case 2:
+            title = "Both"
+        default:
+            title = "None"
+        }
+        
+        return title
+    }
 }
 
