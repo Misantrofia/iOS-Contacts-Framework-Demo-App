a5e31f3a6e08995b470e2c938e8de62c609e64a0
Alexandru Schwartz <alexandru.schwartz@softvision.ro>
2016-07-29 19:16:00 +0300

    Added multiple search + contact save logic

--- iOS-Contacts-Framework-Demo-App/iOS-Contacts-Framework-Demo-App/ViewController.swift
+++ iOS-Contacts-Framework-Demo-App/iOS-Contacts-Framework-Demo-App/ViewController.swift
@@ -16,12 +16,17 @@ enum PilotNumberType {
     case Both
 }
 
+// @"Outgoing Sideline Call" : @"Incoming Sideline Call";
+
 class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
     
     @IBOutlet weak var pilotNumberTypePicker: UIPickerView!
-    @IBOutlet weak var contactNameTextView: UITextView!
+
+    @IBOutlet weak var contactNameTextField: UITextField!
     @IBOutlet weak var inboundPilotNumberLabel: UILabel!
     @IBOutlet weak var outboundPilotNumberLabel: UILabel!
+    @IBOutlet weak var inboundPilotNumberTextField: UITextField!
+    @IBOutlet weak var outboundPilotNumberTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,7 +36,6 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
         self.pilotNumberTypePicker.selectRow(0, inComponent: 0, animated: false)
     }
 
-    
     // MARK: - Actions
 
     @IBAction func showDeviceContactsTaped(sender: AnyObject, forEvent event: UIEvent) {
@@ -43,6 +47,48 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     
     @IBAction func decorateContactButtonTapped(sender: AnyObject) {
         print("Selected contact type is \(self.pilotNumberTypePicker.selectedRowInComponent(0))")
+        
+        let searchText = self.contactNameTextField.text ?? ""
+        
+        print("Unified contacts")
+        let contacts = self.searchContactsWithName(searchText)
+
+        print("Fetched contacts")
+        let filteredContacts = self.searchContactWithMatcingName(searchText)
+
+    }
+    
+    @IBAction func updatePilotNumberButtonTaped(sender: AnyObject, forEvent event: UIEvent) {
+    }
+    
+    @IBAction func placeCallButtonTaped(sender: AnyObject) {
+        
+        let identifier : String
+        
+        switch self.pilotNumberTypePicker.selectedRowInComponent(0) {
+        case 0:
+            identifier = "9F8055C6-9356-445E-AAB5-1F3F491394E9"
+        case 1:
+            identifier = "020124CC-BF2C-45B4-A21B-531CBDC6C75C"
+        case 2:
+            identifier = "F611F5E0-2F17-428C-8A37-AE939930FC78"
+        default:
+            identifier = "F611F5E0-2F17-428C-8A37-AE939930FC78"
+        }
+        
+        print("Search for ID : \(identifier)")
+        let contact = self.searchContactWithIdentifier(identifier);
+        print("Searched contact by ID : \(contact)")
+        
+        let mutableContact = contact?.mutableCopy() as! CNMutableContact
+        let newEmail = CNLabeledValue(label: nil, value: "aaa.bbb@pinger.com")
+        mutableContact.emailAddresses = [newEmail]
+        
+        let saveRequest = CNSaveRequest()
+        saveRequest.updateContact(mutableContact)
+        let store = CNContactStore()
+        try! store.executeSaveRequest(saveRequest)
+        
     }
     
     // MARK: UIPickerViewDataSource
@@ -51,6 +97,8 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
         return 1
     }
     
+    @IBAction func endCallButtonTaped(sender: AnyObject, forEvent event: UIEvent) {
+    }
     func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         return 3
     }
@@ -72,6 +120,59 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
         }
         
         return title
+    }
+    
+    // MARK: Private 
+    
+    func searchContactsWithName(name : String) -> [CNContact] {
+        let predicate: NSPredicate = CNContact.predicateForContactsMatchingName(name)
+        let keysToFetch = [CNContactGivenNameKey]
+        
+        let store = CNContactStore()
+        let contacts = try! store.unifiedContactsMatchingPredicate(predicate, keysToFetch: keysToFetch)
+        
+        for contact in contacts {
+            print("Contact : \(contact)")
+            print("Identifier : \(contact.identifier)")
+        }
+
+        return contacts
+    }
+    
+    func searchContactWithMatcingName(name : String) -> [CNContact] {
+        let predicate: NSPredicate = CNContact.predicateForContactsMatchingName(name)
+        let keysToFetch = [CNContactGivenNameKey]
+        let fetchRequest = CNContactFetchRequest(keysToFetch: keysToFetch)
+        fetchRequest.predicate = predicate
+        fetchRequest.unifyResults = false
+        
+        let store = CNContactStore()
+        var filteredContacts : [CNContact] = []
+        try! store.enumerateContactsWithFetchRequest(fetchRequest) { (contact, stop) in
+          //  if contact.givenName == name {
+                filteredContacts.append(contact)
+            print("Contact : \(contact)")
+            print("Identifier : \(contact.identifier)")
+          //  }
+        }
+        
+        return filteredContacts
+    }
+    
+    func searchContactWithIdentifier(identifier : String) -> CNContact? {
+       let store = CNContactStore()
+        // CNContactFamilyNameKey, CNContactPhoneNumbersKey, CNContactOrganizationNameKey
+        let keysToFetch = [CNContactGivenNameKey, CNContactEmailAddressesKey]
+        
+        var contact : CNContact?
+        
+        do {
+             contact = try store.unifiedContactWithIdentifier(identifier, keysToFetch: keysToFetch)
+        } catch {
+            print("Eception raised")
+        }
+        
+        return contact
     }
 }
 
